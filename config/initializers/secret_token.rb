e56062f920809c0237e756a6ab393cd5ec2fa5ec
Seth Chisamore <schisamo@opscode.com>
2011-01-28 20:55:21 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRailsMongodb::Application.config.secret_token = '86bc112b060e8c9ab5a1ff92768d5b296fdabcb0c7b64f18313e89e68946e10a97f5cdc25bef30f836e3b796147d48323162bf1cfab40072dde020c4a8cfc952'
